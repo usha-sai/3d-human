703e7ad837ccc05ed4dc13e5b3068199883c8abc
usha-sai <100438216+usha-sai@users.noreply.github.com>
2022-04-05 14:37:56 +0530

    Update server.js

--- server.js
+++ server.js
@@ -7,10 +7,12 @@ app.use(express.json());
 app.use(express.static("express"));
 // default URL for website
 app.use('/', function(req,res){
-    res.sendFile(path.join(__dirname+'/index.html'));
+     res.sendFile(path.join(__dirname+'/index.html'));
     //__dirname : It will resolve to your project folder.
   });
 const server = http.createServer(app);
 const port = 3000;
-server.listen(port);
+app.listen(process.env.PORT || 3000, function(){
+  console.log("Express server listening on port %d in %s mode", this.address().port, app.settings.env);
+});
 console.debug('Server listening on port ' + port);
